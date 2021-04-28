c2513e62676b24f5069409f1d153edc465c3f3bb
Erdo <erdoganpak@gmail.com>
2021-04-28 14:35:45 +0300

    Add log client test for succes and fail statuses

--- tests/logClientTests/logClientTests.cpp
+++ tests/logClientTests/logClientTests.cpp
@@ -7,5 +7,12 @@ extern "C"
 
 TEST(TEST_SUITE, WriteSuccesLogTest)
 {
-    EXPECT_EQ(1, 1);
+    LOG_STATUS status = LOG(TRACE, EXAMPLE_LOG, (TIME_STAMP)0, NULL, 0);
+    EXPECT_EQ(status, LOG_WRITE_SUCCESS);
+}
+
+TEST(TEST_SUITE, WriteFailLogTest)
+{
+    LOG_STATUS status = LOG(TRACE, EXAMPLE_LOG, (TIME_STAMP)0, NULL, 801);
+    EXPECT_EQ(status, LOG_WRITE_DATA_FAILED);
 }
